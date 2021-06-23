ecaf960a1439b0c4fb553731a366993220fcd997
Perpus id <79629442+perpusid@users.noreply.github.com>
2021-06-24 00:21:15 +0700

    tubesKPL

---
+++ perpusId/perpusId/daftarAkun.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace perpusId
+{
+    partial class daftarAkun
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(daftarAkun));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inputNohp = new System.Windows.Forms.TextBox();
+            this.inputNama = new System.Windows.Forms.TextBox();
+            this.inputLibId = new System.Windows.Forms.TextBox();
+            this.inputEmail = new System.Windows.Forms.TextBox();
+            this.inputPass = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tglLahir = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(388, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIGN UP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(232, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(233, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(233, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "No.Handphone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(667, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tanggal Lahir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(232, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 28);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Library Id";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(233, 422);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 28);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Password";
+            // 
+            // inputNohp
+            // 
+            this.inputNohp.Location = new System.Drawing.Point(409, 368);
+            this.inputNohp.Name = "inputNohp";
+            this.inputNohp.Size = new System.Drawing.Size(185, 34);
+            this.inputNohp.TabIndex = 4;
+            // 
+            // inputNama
+            // 
+            this.inputNama.Location = new System.Drawing.Point(409, 267);
+            this.inputNama.Name = "inputNama";
+            this.inputNama.Size = new System.Drawing.Size(185, 34);
+            this.inputNama.TabIndex = 2;
+            // 
+            // inputLibId
+            // 
+            this.inputLibId.Location = new System.Drawing.Point(409, 214);
+            this.inputLibId.Name = "inputLibId";
+            this.inputLibId.Size = new System.Drawing.Size(185, 34);
+            this.inputLibId.TabIndex = 1;
+            // 
+            // inputEmail
+            // 
+            this.inputEmail.Location = new System.Drawing.Point(409, 321);
+            this.inputEmail.Name = "inputEmail";
+            this.inputEmail.Size = new System.Drawing.Size(185, 34);
+            this.inputEmail.TabIndex = 3;
+            // 
+            // inputPass
+            // 
+            this.inputPass.Location = new System.Drawing.Point(409, 416);
+            this.inputPass.Name = "inputPass";
+            this.inputPass.Size = new System.Drawing.Size(185, 34);
+            this.inputPass.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(449, 476);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(94, 37);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tglLahir
+            // 
+            this.tglLahir.Location = new System.Drawing.Point(635, 265);
+            this.tglLahir.Name = "tglLahir";
+            this.tglLahir.Size = new System.Drawing.Size(193, 34);
+            this.tglLahir.TabIndex = 5;
+            // 
+            // daftarAkun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(896, 525);
+            this.Controls.Add(this.tglLahir);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.inputPass);
+            this.Controls.Add(this.inputEmail);
+            this.Controls.Add(this.inputLibId);
+            this.Controls.Add(this.inputNama);
+            this.Controls.Add(this.inputNohp);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "daftarAkun";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.daftarAkun_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox inputNohp;
+        private System.Windows.Forms.TextBox inputNama;
+        private System.Windows.Forms.TextBox inputLibId;
+        private System.Windows.Forms.TextBox inputEmail;
+        private System.Windows.Forms.TextBox inputPass;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DateTimePicker tglLahir;
+    }
+}
+
---
+++ perpusId/perpusId/daftarAkun.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace perpusId
+{
+    public partial class daftarAkun : Form
+    {
+        MySqlConnection connection = new MySqlConnection("server = localhost; uid = root; database = tugasakhir");
+
+        public daftarAkun()
+        {
+            InitializeComponent();
+        }
+        private void daftarAkun_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void ValidasiRegister()
+        {
+
+        }
+        public void setNull()
+        {
+            inputLibId.Text = "";
+            inputEmail.Text = "";
+            inputNama.Text = "";
+            inputNohp.Text = "";
+            inputPass.Text = "";
+            tglLahir.Text = "";
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd;
+            connection.Open();
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO member(libraryId,nama,noHp,email,tgl_lahir,password) VALUE(@libId,@nama,@noHp,@email,@tgllhr,@pass)";
+                cmd.Parameters.AddWithValue("@libId", inputLibId.Text);
+                cmd.Parameters.AddWithValue("@nama", inputNama.Text);
+                cmd.Parameters.AddWithValue("@noHp", inputNohp.Text);
+                cmd.Parameters.AddWithValue("@email", inputEmail.Text);
+                cmd.Parameters.AddWithValue("@tgllhr", tglLahir.Value);
+                cmd.Parameters.AddWithValue("@pass", inputPass.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil Membuat Akun");
+                setNull();
+                connection.Close();
+
+            } catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.GetBaseException());
+            }
+        }
+    }
+}
